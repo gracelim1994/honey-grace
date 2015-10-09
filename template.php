dbb8e5dbba4c4d20df3778bc172746b7af3fc3f1
Mary Grace Lim <cutie_mhiemii@yahoo.com>
2015-10-09 22:57:54 +0800

    Create template.php

---
+++ template.php
@@ -0,0 +1,45 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title><?php echo $title; ?></title>
+		<link rel="stylesheet" type="text/css" href="Stylesheet.css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="banner">
+		</div>
+		
+	<nav id="navigation">
+		<ul id='nav'>
+				<li><a href='content.php'>Home</a></li>
+				<li><a href='product.php'>Product</a></li>
+				<li><a href='service.php'>Services</a></li>
+				<li><a href='index.php'>Logout</a></li>
+		</ul>
+	</nav>
+	
+		
+		<div id="content_area">
+			<?php echo $content; ?>
+		</div>	
+		
+		<div id="sidebar">
+			<img src = "images/111.jpg" height="400px" width="200px">
+		</div>
+		
+		
+		<footer>
+			CHOCOLATE</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+			CARAMEL</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+			VANILLA</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+			STRAWBERRY</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+			RED VELVET</a>&nbsp;&nbsp;&nbsp;&nbsp;
+			<br><span>&copy; Honey@Grace Shop</span>
+		</footer>
+		
+		</div>		
+
+	</body>
+</html>
+
+
