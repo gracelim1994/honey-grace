4c5159868f4b92b0fd913e5d5048003f07ec574f
Mary Grace Lim <cutie_mhiemii@yahoo.com>
2015-10-09 22:57:16 +0800

    Create service.php

---
+++ service.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		
+		<title>Ericka@Grace Shop</title>
+		<link rel="stylesheet" type="text/css" href="Stylesheet.css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="banner">
+		</div>
+		
+	<nav id="navigation">
+		<ul id='nav'>
+				<li><a href='content.php'>Home</a></li>
+				<li><a href='product.php'>Product</a></li>
+				<li><a href='service.php'>Services</a></li>
+				<li><a href='index.php'>Logout</a></li>
+		</ul>
+	</nav>
+		
+<div id="content_area">
+	<center><img src = "images/service.jpg">
+</div>	
+	
+		<footer>
+			CHOCOLATE&nbsp;&nbsp;|&nbsp;&nbsp;
+			CARAMEL&nbsp;&nbsp;|&nbsp;&nbsp;
+			VANILLA&nbsp;&nbsp;|&nbsp;&nbsp;
+			STRAWBERRY&nbsp;&nbsp;|&nbsp;&nbsp;
+			RED VELVET&nbsp;&nbsp;&nbsp;&nbsp;
+			<br><span>&copy; Honey@Grace Shop</span>
+		</footer>		
+	</div>		
+</body>
+</html>
+
+
